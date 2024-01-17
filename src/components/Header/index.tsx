eadf0e752ed4da02ccfe442422ebfa399d24acc7
Marinho <alesandra.i.marinho@accenture.com>
2024-01-16 22:01:31 -0300

    Componente reutilizavel de title e subtitle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import { Container, Logo } from './styles'
-import logoImg from '@assets/logo.png'
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-export function Header() {
-    return(
-        <Container>
-            <Logo source={logoImg} />
-        </Container>
-    )
+import logoImg from '@assets/logo.png';
+
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      { 
+        showBackButton &&
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      }
+
+      <Logo source={logoImg} />
+    </Container>
+  )
 }
